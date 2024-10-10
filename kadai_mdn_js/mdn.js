0a0b3f53e732322b0770c8c3a723a9aea73397d9
takapon-design <yd64tk94@gmail.com>
2024-10-10 11:25:32 +0900

    kadai_mdn_jsをアップロードし直します

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,9 @@
-const date = new Date();
+function formatDate(date){
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}年${month}月${day}日`;
+}
 
-console.log(date);
+const now = new Date();
+console.log(formatDate(now));
